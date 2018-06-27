7fbbfc2f73bce6e3a4cd99c3c396f14f06d7710e
sunghsukuo <sunghsukuo@gmail.com>
2018-06-27 23:04:39 +0800

    developing

--- plugins/internal/usonicPlugin.js
+++ plugins/internal/usonicPlugin.js
@@ -44,11 +44,12 @@ function getDistance() {
         triggerGpio.writeSync(0);
         let begin = process.hrtime();
         // detect echo pull-high signal
-        while (echoGpio.readSync() == 0) {
-
+        while ((echoGpio.readSync() == 0) && (timeout < resources.pi.sensors.ultrasonic.timeout)) {
+            let diff = process.hrtime(begin);
+            timeout = (diff[0] * 1e9 + diff[1]) * 1000; // us
         }
-        let diff = process.hrtime(begin);
-        console.log(diff);
+        
+        console.log(timeout);
         /*
         let begin = process.hrtime();
         while ((echoGpio.readSync() == 0) && (resources.pi.sensors.ultrasonic.timeout > timeout)) {
