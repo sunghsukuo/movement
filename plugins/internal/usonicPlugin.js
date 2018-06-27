74437083f16680514124d4fb9c11719756db1b01
sunghsukuo <sunghsukuo@gmail.com>
2018-06-28 07:06:20 +0800

    developing

--- plugins/internal/usonicPlugin.js
+++ plugins/internal/usonicPlugin.js
@@ -2,7 +2,7 @@ let resources = require('./../../resources/model');
 
 let model = resources.pi.sensors.ultrasonic;
 let pluginName = resources.pi.sensors.ultrasonic.name;
-let localParams = {simulate: false, frequency: 1000};
+let localParams = {simulate: false, frequency: 200};
 let interval;
 let echoGpio, triggerGpio;
 
