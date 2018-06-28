91dcd168703041595d553ac13289ecf4772183a4
sunghsukuo <sunghsukuo@gmail.com>
2018-06-28 21:00:18 +0800

    modified on 2018/06/28 週四_21:00:16.69

--- resources/model.js
+++ resources/model.js
@@ -3,5 +3,6 @@ let resources = require('./resources.json');
 
 const handler = {};
 resources.pi.actuators.rgbLEDs[1] = new Proxy(resources.pi.actuators.rgbLEDs[1], handler);
+resources.pi.actuators.rgbLEDs[1].handler = handler;
 
 module.exports = resources;
