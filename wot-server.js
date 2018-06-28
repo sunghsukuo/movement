1e0d1de05097124d81806006c31647cfde60e9d9
sunghsukuo <sunghsukuo@gmail.com>
2018-06-28 20:56:49 +0800

    modified on 2018/06/28 週四_20:56:47.49

--- wot-server.js
+++ wot-server.js
@@ -7,7 +7,7 @@ let rgbLED = require('./plugins/internal/rgbLEDPlugin');
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
-usonicPlugin.start({'simulate': false, 'frequency': 150});
+//usonicPlugin.start({'simulate': false, 'frequency': 150});
 rgbLED.start({'simulate': false, 'frequency': 1000});
 
 
