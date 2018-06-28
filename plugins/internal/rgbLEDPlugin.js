409cb9b10f2a9ac68053469ca1a534f6d4fd99f3
sunghsukuo <sunghsukuo@gmail.com>
2018-06-28 16:21:19 +0800

    modified on 2018/06/28 週四_16:21:18.06

---
+++ plugins/internal/rgbLEDPlugin.js
@@ -0,0 +1,69 @@
+let resources = require('./../../resources/model');
+    utils = require('./../../utils/utils.js');
+
+let model = resources.pi.actuators.rgbLeds['1'];
+let pluginName = model.name;
+let localParams = {'simulate': false, 'frequency': 2000};
+let interval;
+let ledRed, ledGreen, ledBlue;
+
+exports.start = function (params) {
+    localParams = params;
+    observe(model);
+  
+    if (localParams.simulate) {
+      simulate();
+    } else {
+      connectHardware();
+    }
+  };
+
+  exports.stop = function () {
+    if (localParams.simulate) {
+      clearInterval(interval);
+    } else {
+        ledRed.unexport();
+        ledGreen.unexport();
+        ledBlue.unexport();
+    }
+    console.info('%s plugin stopped!', pluginName);
+  };
+
+  function observe(what) {
+    Object.observe(what, function (changes) {
+      console.info('Change detected by plugin for %s...', pluginName);
+      changeRGBColor(model.value); //#B
+    });
+  };
+
+
+  function changeRGBColor(value) {
+    if (!localParams.simulate) {
+        ledRed.pwmWrite(model.value.r);
+        ledGreen.pwmWrite(model.value.g);
+        ledBlue.pwmWrite(model.value.b);
+    }
+  };
+
+  function connectHardware() {
+    var Gpio = require('pigpio').Gpio;
+
+    ledRed = new Gpio(model.gpio.r, {mode: Gpio.OUTPUT});
+    ledGreen = new Gpio(model.gpio.g, {mode: Gpio.OUTPUT});
+    ledBlue = new Gpio(model.gpio.b, {mode: Gpio.OUTPUT});
+
+    //RESET RGB LED
+    ledRed.digitalWrite(0);
+    ledGreen.digitalWrite(0);
+    ledBlue.digitalWrite(0);
+
+    console.info('Hardware %s actuator started!', pluginName);
+  };
+
+  function simulate() {
+    interval = setInterval(function () {
+      // Switch value on a regular basis
+      model.value = {"r":utils.randomInt(0,255), "g":utils.randomInt(0,255), "b":utils.randomInt(0,255)};
+    }, localParams.frequency);
+    console.info('Simulated %s actuator started!', pluginName);
+  };
--- wot-server.js
+++ wot-server.js
@@ -3,10 +3,12 @@ let httpServer = require('./servers/http'),
 
 // Internal Plugins
 let usonicPlugin = require('./plugins/internal/usonicPlugin');
+let rgbLED = require('./plugins/internal/rgbLEDPlugin');
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
 //usonicPlugin.start({'simulate': false, 'frequency': 150});
+rgbLED.start({'simulate': true, 'frequency': 1000});
 
 let server = httpServer.listen(resources.pi.port, function () {
     console.info('Your WoT pi is up and running on port %s', resources.pi.port);
