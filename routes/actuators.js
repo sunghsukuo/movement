282797ab70c81ff4341faa184d1c4c3e6ede94a6
sunghsukuo <sunghsukuo@gmail.com>
2018-06-28 14:04:47 +0800

    modified on 2018/06/28 週四_14:04:46.78

--- routes/actuators.js
+++ routes/actuators.js
@@ -19,8 +19,8 @@ router.route('/rgbLEDs/:id').get(function (req, res, next) {
     //next();
 }).put(function(req, res, next) {
     let selectedLed = resources.pi.actuators.rgbLEDs[req.params.id];
-    console.info(req.body);
-    
+    console.info(req.form);
+
     res.send(selectedLed);
 
     //req.result = selectedLed;
