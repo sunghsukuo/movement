548632721420b8af0183455a54461d6a44e11a13
sunghsukuo <sunghsukuo@gmail.com>
2018-06-28 10:57:02 +0800

    modified on 2018/06/28 週四_10:57:01.01

--- wot-server.js
+++ wot-server.js
@@ -6,7 +6,7 @@ let usonicPlugin = require('./plugins/internal/usonicPlugin');
 
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
-usonicPlugin.start({'simulate': true, 'frequency': 150});
+usonicPlugin.start({'simulate': false, 'frequency': 150});
 
 let server = httpServer.listen(resources.pi.port, function () {
     console.info('Your WoT pi is up and running on port %s', resources.pi.port);
