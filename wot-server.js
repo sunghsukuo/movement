16034f9c6bf87c2d7d7f779894d5229af92e00d7
sunghsukuo <sunghsukuo@gmail.com>
2018-06-28 17:22:02 +0800

    modified on 2018/06/28 週四_17:22:01.25

--- wot-server.js
+++ wot-server.js
@@ -8,7 +8,7 @@ let rgbLED = require('./plugins/internal/rgbLEDPlugin');
 // Internal Plugins for sensors/actuators connected to the PI GPIOs
 // If you test this with real sensors do not forget to set simulate to 'false'
 //usonicPlugin.start({'simulate': false, 'frequency': 150});
-rgbLED.start({'simulate': false, 'frequency': 1000});
+//rgbLED.start({'simulate': false, 'frequency': 1000});
 
 let server = httpServer.listen(resources.pi.port, function () {
     console.info('Your WoT pi is up and running on port %s', resources.pi.port);
